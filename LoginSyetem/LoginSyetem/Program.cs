e44239efc2ffb2edd4bf50a1b98404ea003f43b0
BETÜL ALBAYRAK <betol.99tur@gmail.com>
2022-04-05 17:15:38 +0300

    adding MD5Hash and LoginSyatem homeworks

---
+++ LoginSyetem/LoginSyetem/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+/*
+ * this program is written by Betül ALBAYRAK for the encryption homework
+ * Here we create a small login system. 
+ * 1.fucntion we used for Registering new user,
+ * 2. function for login operation. (in this fucntion using SHA256 hash, we controled the user if she/he registered before or not)
+ * other details you can see them in the code
+ */
+namespace LoginSyetem
+{
+    class Program
+    {
+        static List<User> users = new List<User>();
+
+        //register function
+        public static void AddUser(User user) 
+        {
+            string hashedPassword = getHash(user.Password);
+
+            users.Add(new User() { UserName = user.UserName, Password = hashedPassword });
+        }
+
+        //login function
+        public static string Login(User user) 
+        {
+            string hashedPassword = getHash(user.Password);
+            User currentUser = users.Find(c => (c.UserName == user.UserName) && (c.Password == hashedPassword));
+            if (currentUser is null)
+                return "you cannot enter to our website";
+            return "Welcome to our website";
+        } 
+        static void Main(string[] args)
+        {
+            int number;
+
+            do {                        //instructions
+                Console.WriteLine("Hello! For entering new user press 1\n");
+                Console.WriteLine("       For login press 2\n");
+                Console.WriteLine("       For exit press 0\n");
+                 number = int.Parse(Console.ReadLine());
+
+                if (number == 1) {      //Registering new user
+                    Console.WriteLine("Hello! Enter your UserName, then your Password please.\n");
+                    User user = new User();
+                    Console.WriteLine("Enter your UserName:");
+                    user.UserName = Console.ReadLine();
+                    Console.WriteLine("Enter your Password:");
+                    user.Password = Console.ReadLine();
+                    AddUser(user);
+                }
+                else if (number == 2) {  //login
+                    Console.WriteLine("Hello! Enter your UserName, then your Password please.\n");
+                    User user = new User();
+                    Console.WriteLine("Enter your UserName:");
+                    user.UserName = Console.ReadLine();
+                    Console.WriteLine("Enter your Password:");
+                    user.Password = Console.ReadLine();
+                    string result = Login(user);
+                    Console.WriteLine(result);
+                }
+                else if (number != 0){ Console.WriteLine("Opss, You have entered a wrong number, so enter agian\n"); }
+
+            } while (number != 0);
+        }
+
+        //hash function
+        private static string getHash(string text)
+        {
+            // SHA512 is disposable by inheritance.  
+            using (var sha256 = SHA256.Create())
+            {
+                // Send a sample text to hash.  
+                var hashedBytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(text));
+                // Get the hashed string.  
+                return BitConverter.ToString(hashedBytes).Replace("-", "").ToLower();
+            }
+        }
+
+    }
+    public class User 
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ MD5Hash/MD5Hash/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+/*
+ * this program is written by Betül ALBAYRAK
+ * Here we used MD5Hash on 2 different sentence. Each one of them has 500 character, then we encrypted them and the result was:
+ * Hello!
+    Your 1.sentence is:
+    1. original str:
+    MGKBMAVFEAZCPZTDATCLGVCGCBSUIXAAXNOFFKUNOMOPETRMMYAFIWHREDYLHBXHAZFCLNYQZPXTGMCJJGHKXVRAFWBBLJFNPHPEOPBZZKGSZNKRAPBBTMKFHFWOMWEQJKEZXYUUEIKMOYARLJAVHDTCXJWJSMMDFQIPJLUCUHWLCXQIEFCCVIIGQCBNBLBRVGHYQLIAPYBZHIAATOENKKHWHCZGJGBIGEPVPRSSWCMXHJYHCBUNHOHLZKRNLBQJXSTCTEKBVLQIXKPYDSEIMNBSXDWFAFGVLDAPPWNXRSRVXZOOUTJTXIWJQBFPIFBQEVWTKZIHEGRTWGUXPHKHWNXCGQYORYPLRBQCCVEVKBPRRDNKGHZAUUTRXTMTBUPFQFBUPSVUDPWUXGLDDOHKYPRXHYEIIKTQMJAMBSUOLFWFXZURIIQMMFHAOEMQNMLTVFIOFYBKTTYSTLZIENAZTGEHCAYXGDBZTDJSRFMJGGTTGOHCEEM
+    1. Hashed str:
+    56DFA1F2A81FB7722B8F190B96FBA698
+
+
+
+    Your 2.sentence is:
+    2. original str:
+    ADTOBVXJIMTYMPAVARSXDZGIQXXGQKHQLECFLADZCHKWPFOJNZFHWPEAWKYDXRTYFDMIMUJANSVVPKSLJOSQFZTWABIACZBLNMRMSGWTPETEUILMSMUJYDSFJQIKMJYOQJIDTGSDRZAAGPBRQVIPFSMKTLDCXLGGFFNKZITMUQRZCRZTOQUQAJIXWYJHDPFGSTUBBRSICWUOBMHCMJOHRQGWWPNBYLJFLLHIWXSXXLNEPTWKQHVTBXOCNYELDQKRMPDPFYFOQQJFCBHDTDKQPPDOWFVYTGUGBWRZQGERCHDKNIZMRBSZGTBXLOTZDHVKWCEFJHMMYSOIMTANFIGROUAIVUOBNEOULSNKEUCJOFANESLROFUUAEWBCWKOSLZTEUXTGQJLGWCIUNDCJXNHKTUHWLIIJNRDGGIKVSLXWJODBNHPSHFVXRJLZPWUHBBSFYBNETHQLPLBEQGIESPCHVGOXXKFVRPVHWXCLTLZZPKHFHHIJNQ
+    2. Hashed str:
+    63E2A32239873606F2D31ADE04D3D7A6
+* my comment is: 
+* As can see Hashed sentences are different, that is because the sentences that we hashed are not the same
+ */
+
+namespace MD5Hash
+{
+    class Program
+    {
+        public static string CreateMD5(string input)
+        {
+            // Use input string to calculate MD5 hash
+            using (System.Security.Cryptography.MD5 md5 = System.Security.Cryptography.MD5.Create())
+            {
+                byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(input);
+                byte[] hashBytes = md5.ComputeHash(inputBytes);
+
+                return Convert.ToHexString(hashBytes);
+            }
+        }
+
+        public static string GenerateString()
+        {
+            // Creating object of random class
+            Random rand = new Random();
+
+            // Choosing the size of string
+            // Using Next() string
+            int stringlen = rand.Next(499, 500);
+            int randValue;
+            string str = "";
+            char letter;
+            for (int i = 0; i < stringlen; i++)
+            {
+
+                // Generating a random number.
+                randValue = rand.Next(0, 26);
+
+                // Generating random character by converting
+                // the random number into character.
+                letter = Convert.ToChar(randValue + 65);
+
+                // Appending the letter to string.
+                str = str + letter;
+            }
+            return str;
+        }
+
+        static void Main(string[] args)
+        {
+            //1.sentence
+            Console.WriteLine("Hello!");
+            Console.WriteLine("Your 1.sentence is:");
+            Console.WriteLine("1. original str:");
+            string currentStr1 = GenerateString();
+            Console.WriteLine(currentStr1);
+            Console.WriteLine("1. Hashed str:");
+            string hashedStr1 = CreateMD5(currentStr1);
+            Console.WriteLine(hashedStr1);
+
+            Console.WriteLine("\n\n");
+            //2.sentence
+            Console.WriteLine("Your 2.sentence is:");
+            Console.WriteLine("2. original str:");
+            string currentStr2 = GenerateString();
+            Console.WriteLine(currentStr2);
+            Console.WriteLine("2. Hashed str:");
+            string hashedStr2 = CreateMD5(currentStr2);
+            Console.WriteLine(hashedStr2);
+
+        }
+    }
+}
